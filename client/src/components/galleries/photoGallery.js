d8387b7b4061c1758afc041f9c2e335ab6374b78
Fronrich Puno <fronrich@utexas.edu>
2020-12-07 04:09:12 -0600

    added tech stack

--- client/src/components/galleries/photoGallery.js
+++ client/src/components/galleries/photoGallery.js
@@ -2,6 +2,8 @@ import React from "react";
 import Gallery from "react-photo-gallery";
 // https://www.npmjs.com/package/react-photo-gallery
 
+// TODO: IMPORT PICTURES FROM IMGUR
+
 export default function photoGallery(props) {
-  return <div></div>;
+  return <Gallery photos={Data} />;
 }
